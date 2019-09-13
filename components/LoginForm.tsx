5a35fda9022239f5e9bc5ee5bb0051c64c701032
Hector <hectordorito5@gmail.com>
2019-09-13 22:26:29 +0200

    mejora login ui

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,50 +1,109 @@
 import React from "react";
-import { View } from "react-native";
-import { withTheme, TextInput, Button } from "react-native-paper";
-
-
+import { View, Dimensions } from "react-native";
+import {
+  withTheme,
+  TextInput,
+  Button,
+  Surface,
+  HelperText,
+  Headline,
+  Title
+} from "react-native-paper";
 
 interface IProps {
-    post: (user: string, password: string) => void
+  post: (user: string, password: string) => void;
 }
 
 interface IState {
-    username: string,
-    password: string
+  username: string;
+  password: string;
 }
 
-
 class LoginForm extends React.PureComponent<IProps, IState> {
-    constructor(props) {
-        super(props);
-        this.state = {username: "", password: ""}
-    }
+  constructor(props) {
+    super(props);
+    this.state = { username: "", password: "" };
+  }
 
-    render() {
+  render() {
+    const { colors, roundness } = this.props.theme;
+    const { width, height } = Dimensions.get("window");
+    return (
+      <View
+        style={{
+          backgroundColor: colors.primary,
+          height: height,
+          flex: 1,
+          justifyContent: "space-between"
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <Headline>Cajamar</Headline>
+        </View>
 
-
-        return (
-            <View style={{ margin: 10 }}>
-
-                <TextInput onChangeText={((text) => this.setState({ username: text }))} value={this.state.username} style={{ marginTop: 10, textColor: "black" }} label="Nombre de usuario" mode="outlined" />
-                <TextInput secureTextEntry={true} onChangeText={((text) => this.setState({ password: text }))} value={this.state.password} style={{ marginTop: 10, textColor: "black" }} label="Contraseña" mode="outlined" />
-                <Button
-                    style={{ marginTop: 10 }}
-                    icon="refresh"
-                    mode="contained"
-                    dark={true}
-                    onPress={() => {
-                        if (this.state.username.length > 0 && this.state.password.length > 0) {
-                            this.props.post(this.state.username, this.state.password)
-                        }
-                    }}
-                >
-                    Conectarse al banco.
-</Button>
-
-            </View>
-        )
-    }
+        <Surface
+          style={{
+            flex: 2,
+            flexDirection: "column",
+            justifyContent: "center",
+            backgroundColor: "white",
+            borderRadius: roundness,
+            elevation: 5,
+            margin: 5,
+            padding: 5
+          }}
+        >
+          <TextInput
+            onChangeText={text => this.setState({ username: text })}
+            value={this.state.username}
+            style={{
+              textColor: "black",
+              backgroundColor: "white"
+            }}
+            label="Nombre de usuario"
+            theme={{ roundness: 0 }}
+          />
+          <HelperText type="error" visible={this.state.username.length === 0}>
+            Este campo es obligatorio
+          </HelperText>
+          <TextInput
+            secureTextEntry={true}
+            onChangeText={text => this.setState({ password: text })}
+            value={this.state.password}
+            style={{
+              marginTop: 10,
+              textColor: "black",
+              backgroundColor: "white"
+            }}
+            label="Contraseña"
+            theme={{ roundness: 0 }}
+          />
+          <HelperText type="error" visible={this.state.password.length === 0}>
+            Este campo es obligatorio
+          </HelperText>
+          <Button
+            style={{ marginTop: 10 }}
+            icon="refresh"
+            mode="contained"
+            dark={true}
+            onPress={() => {
+              if (
+                this.state.username.length > 0 &&
+                this.state.password.length > 0
+              ) {
+                this.props.post(this.state.username, this.state.password);
+              }
+            }}
+          >
+            Conectarse al banco.
+          </Button>
+        </Surface>
+        <View style={{ flex: 1 }}>
+          <Headline>Esto sería el footer</Headline>
+        </View>
+      </View>
+    );
+  }
 }
 
 export default withTheme(LoginForm);
